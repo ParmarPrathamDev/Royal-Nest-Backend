5cc011411f3dbf78cc7ba69cc2ff1dfe48986274
ParmarPrathamDev <parmarpratham1111@gmail.com>
2026-04-29 00:34:51 +0530

    cross-origin-comma

--- server.js
+++ server.js
@@ -19,7 +19,7 @@ app.use(cors({
     origin: [
         "http://localhost:5173",
         "http://localhost:5174",
-        "http://localhost:5175"
+        "http://localhost:5175",
         "https://royal-nest-frontend.vercel.app/"
     ],
     credentials: true
