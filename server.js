e0365a828e2cb2a98db88c562a2a3c6161b358cb
ParmarPrathamDev <parmarpratham1111@gmail.com>
2026-04-29 00:32:07 +0530

    crosorgin

--- server.js
+++ server.js
@@ -20,6 +20,7 @@ app.use(cors({
         "http://localhost:5173",
         "http://localhost:5174",
         "http://localhost:5175"
+        "https://royal-nest-frontend.vercel.app/"
     ],
     credentials: true
 }))
